d271b942aa75d760e432e52999abb880ecb55c54
EduardoGomez06 <115214869+EduardoGomez06@users.noreply.github.com>
2022-11-30 13:54:50 +0100

    Add files via upload

---
+++ Dia_32_proyecto/contactanos/configuration.php
@@ -0,0 +1,10 @@
+<?php
+
+$databaseHost = 'localhost';
+$databaseName = 'bddpublicidad';
+$databaseUsername = 'edgomez';
+$databasePassword = 'Bestbrandgom';
+
+$mysqli = new mysqli($databaseHost, $databaseUsername, $databasePassword, $databaseName);
+
+?>
---
+++ Dia_32_proyecto/home.php
@@ -0,0 +1,36 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/bootstrap@3.4.1/dist/css/bootstrap.min.css">
+    <title>Best Brand</title>
+</head>
+<body>
+<div>
+<h1>Best Brand</h1>
+    <h2>Qué quieres hacer?</h2>
+    <form method="post" action="inicio.php">
+    <label for="luchar"></label>
+    <button name="luchar" id="luchar">Inicio</button>
+    </form>
+    <form method="post" action="nosotros.php">
+    <label for="alimento"></label>
+    <button action="nosotros.php" name="alimento" id="alimento">Sobre nosotros
+    </button>
+    </form>
+    <form method="post" action="planes.php">
+    <label for="entrenamiento"></label>
+    <button action="planes.php" name="entrenamiento" id="entrenamiento">Planes
+    </button>
+    </form>
+    </form>
+    <form method="post" action="./contactanos/contact.html">
+    <label for="entrenamiento"></label>
+    <button action="contactanos.php" name="entrenamiento" id="entrenamiento">Contactanos
+    </button>
+    </form>
+
+</body>
+</html>
